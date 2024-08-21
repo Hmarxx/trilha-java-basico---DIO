2cadca0ab58c0b01e98982550bc28eb99a2d7ae3
Hmarxx <henrymarx@hotmail.com>
2024-08-20 23:39:18 -0300

    Aula de operadores, incrementação e booleanos.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        int numero = 5;
+        boolean variavel = true;
+
+        System.out.println(++numero);
+
+        variavel = !variavel;
+        System.out.println(!variavel);
+
+        }
+    }
